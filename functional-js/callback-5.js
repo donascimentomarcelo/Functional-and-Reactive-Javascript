cc86e8be2b313b4b22138ea9f37e37a66ecac6ac
Marcelo Sant'Anna <marcelojunin2010@hotmail.com>
2022-11-06 11:20:01 -0300

    Basic reduce

---
+++ functional-js/callback-5.js
@@ -0,0 +1,14 @@
+const cart = [
+    { name: 'Pencil', qtd: 10, price: 7.99 },
+    { name: 'Print', qtd: 0, price: 600 },
+    { name: 'Book', qtd: 3, price: 5.82 },
+];
+
+const getTotal = item =>  item.qtd * item.price;
+const sum = (accumulator, element) => accumulator + element;
+
+const total = cart
+    .map(getTotal)
+    .reduce(sum);
+
+console.log(total);
